b8c819c08dd24e24e25de94a0ff9e4db03a7f8d7
krunalyelgire <yelgirekrunal@gmail.com>
2026-07-21 23:49:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const scriptURL = "PASTE_YOUR_WEB_APP_URL_HERE";
 
+function login() {
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  google.script.run
+    .withSuccessHandler(function(response){
+
+      if(response.status == "success"){
+
+        if(response.role == "HR"){
+          window.location.href = "hr-dashboard.html";
+        }
+        else if(response.role == "Employee"){
+          window.location.href = "employee-dashboard.html";
+        }
+
+      } else {
+        alert("Invalid Username or Password");
+      }
+
+    })
+    .checkLogin(username,password);
+
+}
