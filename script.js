e499f24a7fcb91bf0700184316a9a85ae6dbbb90
krunalyelgire <yelgirekrunal@gmail.com>
2026-07-22 01:35:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const webAppURL = "https://script.google.com/macros/s/AKfycbzTtPGTeSQAcgb6Am_yvvkgINZTjo0P0vD0BYPewO4wyartoqC5N0BfRbx7p_fsKGQ/exec";
+const webAppURL = "https://script.google.com/macros/s/AKfycbxEmbL8co7DTKuRn2il1iQ5-0j9m3JEOq_5zhJx0x4iuQYozeOkHrbdXknvS01VqsM36A/exec";
 
 function login() {
 
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  fetch(webAppURL + "?username=" + username + "&password=" + password)
-
+  fetch(webAppURL + "?username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password))
+  
   .then(response => response.json())
 
   .then(data => {
+
+    console.log(data);
 
     if(data.status == "success") {
 
@@ -17,22 +19,29 @@ function login() {
       localStorage.setItem("role", data.role);
 
       if(data.role == "HR") {
+
         window.location.href = "hr-dashboard.html";
-      }
-      else {
+
+      } else {
+
         window.location.href = "employee-dashboard.html";
+
       }
 
     } 
     else {
+
       document.getElementById("message").innerHTML = "Invalid Username or Password";
+
     }
 
   })
 
   .catch(error => {
+
     console.log(error);
     document.getElementById("message").innerHTML = "Server connection error";
+
   });
 
 }
